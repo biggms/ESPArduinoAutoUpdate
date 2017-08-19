31f59e77a2bb8f4feae7ca21b5bc6d09febf86ef
Graham Stewart <graham.stewart@tcsjohnhuxley.com>
2017-08-19 23:58:09 +0100

    Initial checkin

---
+++ AutoUpdate.cpp
@@ -0,0 +1,37 @@
+#include "Arduino.h"
+#include "AutoUpdate.h"
+#include <ArduinoOTA.h>
+
+AutoUpdate::AutoUpdate( const char* pPassword, const char* pHostname )
+{
+  ArduinoOTA.setPassword(pPassword);
+  ArduinoOTA.setHostname(pHostname);
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    ESP.restart();
+  });
+}
+
+void AutoUpdate::start()
+{
+  ArduinoOTA.begin();
+}
+
+void AutoUpdate::loop()
+{
+  ArduinoOTA.handle();
+}
