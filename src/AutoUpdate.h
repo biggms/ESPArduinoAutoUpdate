c4079629246693c6a8f6448237eccb955fbcfd72
Graham Stewart <graham.stewart@tcsjohnhuxley.com>
2017-08-20 00:33:08 +0100

    Reformatted and moved includes

--- src/AutoUpdate.h
+++ src/AutoUpdate.h
@@ -1,14 +1,16 @@
 #include "Arduino.h"
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
 
 #ifndef AutoUpdate_h
 #define AutoUpdate_h
 
 class AutoUpdate
 {
-  public:
-    AutoUpdate( const char* pPassword, const char* pHostname );
-    void start();
-    void loop();
+public:
+AutoUpdate( const char* pPassword, const char* pHostname );
+void start();
+void loop();
 };
 
 #endif
